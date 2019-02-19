8fa09b0e7507311dce7b7319a02e03a403c9dfba
Kevin Welch <kaedub16@gmail.com>
2019-02-18 23:44:06 -0800

    Changed conditionals to match statement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,33 +1,31 @@
 use std::io;
+use std::cmp::Ordering;
+
 // use rand::Rng;
 
 fn main() {
     // create a random unsigned 8 byte integer
-    let secret = rand::random::<u8>();
+    let secret_number = rand::random::<u8>();
     println!("Guess the number!");
     println!("Please input your guess.");
-    println!("**The number is {}**", secret);
+    println!("**The number is {}**", secret_number);
 
     // create mutable string
-    let mut input_text = String::new();
+    let mut guess = String::new();
 
     // read standard input
     io::stdin()
-        .read_line(&mut input_text)
+        .read_line(&mut guess)
         .expect("Failed to read line");
 
     // trim white space, parse integer, and handle error
-    let guess = input_text
-        .trim()
-        .parse()
-        .expect("Expected a number");
+    let  guess: u8 = guess.trim().parse()
+        .expect("Expected a number to be input");
 
-    if secret == guess {
-        println!("Correct!");
-        println!("You guessed: {}", guess);
-    } else {
-        println!("Sorry, that was incorrect.");
-        println!("You guessed: {}", guess);
-        println!("The answer was: {}", secret);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
     }
+    println!("The number is {}", secret_number)
 }
