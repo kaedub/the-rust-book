1398f704835379f830b2e409d410e7cf55235497
Kevin Welch <kaedub16@gmail.com>
2019-02-20 00:27:43 -0800

    Finished Rust chapter 3

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -19,14 +19,78 @@ fn main() {
 
     println!("number is {}", number);
 
-    loop {
-        println!("if_expression returns {}", if_expression());
-    }
+    println!("if_expression returns {}", if_expression());
+    
+
+    println!("ex_loop_with_break returns {}", ex_loop_with_break());
+    println!("ex_loop_while returns {}", ex_loop_while());
+    ex_loop_for();
+    ex_rev_for();
+
+    // infite loop
+    // loop {
+    //     println!("if_expression returns {}", if_expression());
+    // }
+
+
+            /* Notable Errors */
+
     // Rust does not do type coercion
     // error[E0308]: mismatched types
     // if number {
     //     println!("condition was true");
     // }
+}
+
+fn ex_rev_for() {
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn ex_loop_for() {
+    let a = [1,2,3,4];
+
+    for element in a.iter() {
+        println!("a[i]={}", element)
+    }
+}
+
+fn ex_loop_while() -> u8 {
+    let mut number = 3;
+    while number > 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("Liftoff!");
+    number
+}
+
+// fn ex_while() {
+//     let mut number = 3;
+
+    // while number !=  {
+    //     println!("{}!", number);
+    //     number = number - 1;
+    // }
+    // println!("Liftoff...");
+// }
+
+fn ex_loop_with_break() -> u32 {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("result is {} and counter is {}", result, counter);
+    assert_eq!(result, 20);
+    return counter;
 }
 
 // interesting
