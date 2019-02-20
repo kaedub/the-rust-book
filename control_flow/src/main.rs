2684d9a42ca582159c1e2b6e8497d901086a75b7
Kevin Welch <kaedub16@gmail.com>
2019-02-20 00:14:42 -0800

    Started loops

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,33 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
+    /* 
+    if expressions... 
+    yes they are if expressios
+    not if statements
+    */
+    if number == 0 {
+        println!("number is zero");
+    } else if number < 4 {
+        println!("number is less than four");
     } else {
-        println!("condition was false");
+        println!("number is four or greater");
     }
+
+    // this should return 3
+    let number = if 3 > 4 { 2 } else { 3 };
+
+    println!("number is {}", number);
+
+    loop {
+        println!("if_expression returns {}", if_expression());
+    }
+    // Rust does not do type coercion
+    // error[E0308]: mismatched types
+    // if number {
+    //     println!("condition was true");
+    // }
 }
+
+// interesting
+fn if_expression() -> u32 { if 3 > 4 { 10 } else { 12 } }
