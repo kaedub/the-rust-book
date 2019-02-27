8e805efd330b6ee9bca795103f6d175a61ab941c
Kevin Welch <kaedub16@gmail.com>
2019-02-26 23:24:53 -0800

    return values and scope

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,8 +10,18 @@ fn main() {
                                     // but i32 is Copy, so it’s okay to still
                                     // use x afterward
 
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
 } // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens.
+  // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
+  // moved, so nothing happens. s1 goes out of scope and is dropped.
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("{}", some_string);
@@ -21,3 +31,21 @@ fn takes_ownership(some_string: String) { // some_string comes into scope
 fn makes_copy(some_integer: i32) { // some_integer comes into scope
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
